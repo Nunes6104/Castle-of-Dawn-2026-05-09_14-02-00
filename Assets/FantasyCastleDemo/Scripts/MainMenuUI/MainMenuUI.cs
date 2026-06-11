69bb0b42a087b6500d2926b402ff99cd9c6e7956
Leofer <leo7.fernandes@gmail.com>
2026-06-11 22:14:53 +0100

    menu melhor

--- Assets/FantasyCastleDemo/Scripts/MainMenuUI/MainMenuUI.cs
+++ Assets/FantasyCastleDemo/Scripts/MainMenuUI/MainMenuUI.cs
@@ -1,11 +1,30 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour
 {
+    public GameObject settingsPanel;
+    public Slider volumeSlider;
+
     public void StartGame()
     {
         SceneManager.LoadScene("SampleScene");
+    }
+
+    public void OpenSettings()
+    {
+        settingsPanel.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        settingsPanel.SetActive(false);
+    }
+
+    public void SetVolume(float value)
+    {
+        AudioListener.volume = value;
     }
 
     public void QuitGame()
